1d4ef0633239f22475aecb41797e5fdc48db8646
leokster <info@timrohner.ch>
2023-06-08 16:12:52 +0200

    play on all sonos

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# DingDong
+# DinDong
+
+This is a Python script that allows you to play a DingDong on a Sonos speaker using a local HTTP server. The script uses the soco library to control the Sonos speaker and the click library for command line interface.
+
+
+# How to use it
+
+```
+pip install dingdong-sonos
+dingdong
+```
+
+You can adjust the volume or set the sonos_ip:
+```
+dingdong --sonos_ip <ip-of-your-sonos> --volume 80
--- dingdong_sonos/main.py
+++ dingdong_sonos/main.py
@@ -1,3 +1,4 @@
+import soco
 import click
 import os
 from soco import SoCo
@@ -26,10 +27,29 @@ def start_http_server(port="8001"):
     return httpserver
 
 
+def group_all_sonos():
+    # Discover all Sonos speakers
+    speakers = list(soco.discover())
+
+    # Get the first speaker as the coordinator
+    coordinator = speakers[0]
+
+    # Join all other speakers to the coordinator
+    for speaker in speakers[1:]:
+        speaker.join(coordinator)
+
+    # Print the group information
+    print("Sonos speakers grouped successfully.")
+
+
 @click.command()
-@click.argument("sonos_ip")
+@click.option(
+    "--sonos_ip",
+    default=None,
+    help="IP address of the Sonos speaker, default is to us ALL speakers",
+)
 @click.option("--port", default="8001", help="Port to run the HTTP server on")
-@click.option("--volume", default=5, help="Volume to play the sound at")
+@click.option("--volume", default=60, help="Volume to play the sound at")
 def main(sonos_ip, port, volume):
     # Get the IP of this machine in the local network
     host_address = f"{get_local_ip()}:{port}"
@@ -42,8 +62,13 @@ def main(sonos_ip, port, volume):
     # Start the HTTP server
     httpserver = start_http_server(port)
 
-    # Create a SoCo instance
-    sonos = SoCo(sonos_ip)
+    if sonos_ip:
+        sonos = SoCo(sonos_ip)
+        sonos.volume = volume
+    else:
+        group_all_sonos()
+        sonos = list(soco.discover())[0]
+        sonos.group.volume = volume
 
     # Create a Resource object for the MP3 file
     mp3_resource = DidlResource(
@@ -64,7 +89,6 @@ def main(sonos_ip, port, volume):
     # Add the MP3 file to the Sonos speaker's queue
     sonos.add_to_queue(mp3_item)
 
-    sonos.volume = volume
     # Play the queue (i.e., the MP3 file we just added)
     sonos.play_from_queue(0)
 
